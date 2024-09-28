6b0294cce5a704eed9c50700c0345c1cbbcd828e
moshiur <moshi.rahman>
2024-09-28 23:23:19 +0600

    first push with basic setup

---
+++ src/Wpm.Management.Api/Controllers/BreedsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreedsController(ManagementDbContext dbContext, ILogger<BreedsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var all = await dbContext.Breeds.ToListAsync();
+        return all != null ? Ok(all) : NotFound();
+    }
+
+    [HttpGet("{id}", Name = nameof(GetBreedById))]
+    public async Task<IActionResult> GetBreedById(int id)
+    {
+        var breed = await dbContext.Breeds.
+            Where(p => p.Id == id)
+            .FirstOrDefaultAsync();
+        return breed != null ? Ok(breed): NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> create(NewBreed newBreed)
+    {
+        try
+        {
+            var breed = newBreed.toBreed();
+            await dbContext.Breeds.AddAsync(breed);
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtRoute(nameof(GetBreedById), new { id = breed.Id }, newBreed);
+        }
+        catch (Exception e)
+        {
+            logger?.LogError(e.ToString());
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
+
+public record NewBreed(string Name)
+{
+    public Breed toBreed()
+    {
+        return new Breed(0, Name);
+    }
+}
---
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PetsController(ManagementDbContext dbContext, Logger<PetsController> logger) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var all = await dbContext.Pets.Include(p => p.Breed).ToListAsync();
+        return Ok(all);
+    }
+
+    [HttpGet("{id}", Name = nameof(GetPetById))]
+    public async Task<IActionResult> GetPetById(int id)
+    {
+        var pet = await dbContext.Pets.Include(p => p.Breed).
+            Where(p => p.Id == id)
+            .FirstOrDefaultAsync();
+        return Ok(pet);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> create(NewPet newPet)
+    {
+        try
+        {
+            var pet = newPet.toPet();
+            await dbContext.Pets.AddAsync(pet);
+            await dbContext.SaveChangesAsync();
+
+            return CreatedAtRoute(nameof(GetPetById), new { id = pet.Id }, newPet);
+        } catch (Exception e)
+        {
+            logger?.LogError(e.ToString());
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
+
+public record NewPet(string Name, int Age, int BreedId)
+{
+    public Pet toPet()
+    {
+        return new Pet() { Name = Name, Age = Age, BreedId = BreedId };
+    }
+}
