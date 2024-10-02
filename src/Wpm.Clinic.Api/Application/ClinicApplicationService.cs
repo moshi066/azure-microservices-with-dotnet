aab32430659b09cfbc9ca577cf7e8bd5977b957b
moshiur <moshi.rahman>
2024-10-02 11:49:14 +0600

    added clinic repo and also the github action

---
+++ src/Wpm.Clinic.Api/Application/ClinicApplicationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Wpm.Clinic.Controllers;
+using Wpm.Clinic.DataAccess;
+using Wpm.Clinic.ExternelServices;
+
+namespace Wpm.Clinic.Application;
+
+public class ClinicApplicationService(ClinicDbContext dbContext,
+    ManagementService managementService)
+{
+    public async Task<Consultation> Handle(StartConsultationCommand command)
+    {
+        var petInfo = await managementService.GetPetInfo(command.PatientId);
+        var newConsultaion = new Consultation(Guid.NewGuid(),
+                                              command.PatientId,
+                                              petInfo.Name,
+                                              petInfo.Age,
+                                              DateTime.UtcNow);
+
+        await dbContext.Consultations.AddAsync(newConsultaion);
+        await dbContext.SaveChangesAsync();
+        return newConsultaion;
+    }
+}
+
---
+++ src/Wpm.Clinic.Api/Controllers/ClinicController.cs
@@ -0,0 +1,19 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Wpm.Clinic.Application;
+
+namespace Wpm.Clinic.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClinicController(ClinicApplicationService applicationService) : ControllerBase
+{
+    [HttpPost("/start")]
+    public async Task<IActionResult> Start(StartConsultationCommand command)
+    {
+        var result = await applicationService.Handle(command);
+        return Ok(result);
+    }
+}
+
+public record StartConsultationCommand(int PatientId);
